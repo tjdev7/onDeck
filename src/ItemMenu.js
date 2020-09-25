c031bf8f77042e62ab75b7a23bffcd9bd047f846
Toughee <MJ1menez@protonmail.com>
2020-09-24 23:28:17 -0500

    onDeck/006/UI_Drafting_4 - Added more UI elements and components for app - Worked on app layout for components

--- src/ItemMenu.js
+++ src/ItemMenu.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const menu items
+function MenuItems {
+	
+}
 
 
 const LikeButton {
-
+	return (
+		<i className = "fa fa-thumbs-up"/>
+	);
 }
 
 Const UnlikeButton {
-
+	return (
+		<i className = "fa fa-thumbs-down"/>
+	);
 } 
 
 Const FavoriteButton {
-
+	return (
+		<i className = "fa fa-thumb-tack"/>
+	);
 }
 
 Const HideButton {
-	
+	return (
+		<i className = "fa fa-times-circle"/>
+	);	
 }
