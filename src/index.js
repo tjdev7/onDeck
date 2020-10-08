5e841c62df6d654b086eaf536244683655c56845
Toughee <MJ1menez@protonmail.com>
2020-10-07 23:19:25 -0500

    onDeck/011/Refactoring_01 - Refactored code inside project - Removed unnecesary components and elements inside project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './App.css';
+import './bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+//
 // function CardDisplay {
 // 	return (
 
@@ -25,6 +27,87 @@ ReactDOM.render(
 //         <p className="App-Subtitle">Picture sharing platform</p>
 // 	);
 // }
+
+// function CardRacks {
+// 	return (
+		// <div className="container">
+//         <div className="rowtest1 row">
+//           <span className="col">
+//               <img src={cardstock1} alt="carttest" />
+//               <h1>title test</h1>
+//               // <i className = "fa fa-thumbs-up"/>
+//               // <i className = "fa fa-thumbs-down"/>
+//               // <i className = "fa fa-thumb-tack"/>
+//               // <i className = "fa fa-times-circle"/>
+//           </span>
+
+//           <span className ="col">
+//             <h1>title test</h1>
+//             <img src={cardstock2} alt="carttest" />
+            
+//             // <i className = "fa fa-thumbs-up"/>
+//             // <i className = "fa fa-thumbs-down"/>
+//             // <i className = "fa fa-thumb-tack"/>
+//             // <i className = "fa fa-times-circle"/>
+//           </span>
+        
+//           <span className="col">
+//             <img src={cardstock3} alt="carttest" />
+//             <i className = "fa fa-thumbs-up"/>
+//             <i className = "fa fa-thumbs-down"/>
+//             <i className = "fa fa-thumb-tack"/>
+//             <i className = "fa fa-times-circle"/>
+//           </span>
+        
+//           <span className="col">
+//             <img src={cardstock4} alt="carttest" />
+//             <i className = "fa fa-thumbs-up"/>
+//             <i className = "fa fa-thumbs-down"/>
+//             <i className = "fa fa-thumb-tack"/>
+//             <i className = "fa fa-times-circle"/>
+//           </span>
+//         </div>
+
+//         <div className="rowtest1 row">
+//           <span className="col">
+//               <img src={cardstock5} alt="carttest" />
+//               <i className = "fa fa-thumbs-up"/>
+//               <i className = "fa fa-thumbs-down"/>
+//               <i className = "fa fa-thumb-tack"/>
+//               <i className = "fa fa-times-circle"/>
+//           </span>
+//           <span className="col">
+//               <img src={cardstock6} alt="carttest" />
+//               <i className = "fa fa-thumbs-up"/>
+//               <i className = "fa fa-thumbs-down"/>
+//               <i className = "fa fa-thumb-tack"/>
+//               <i className = "fa fa-times-circle"/>
+//           </span>
+//           <span className="col">
+//               <img src={cardstock7} alt="carttest" />
+//               <i className = "fa fa-thumbs-up"/>
+//               <i className = "fa fa-thumbs-down"/>
+//               <i className = "fa fa-thumb-tack"/>
+//               <i className = "fa fa-times-circle"/>
+//           </span>
+//           <span className="col">
+//               <img src={cardstock8} alt="carttest" />
+//               <i className = "fa fa-thumbs-up"/>
+//               <i className = "fa fa-thumbs-down"/>
+//               <i className = "fa fa-thumb-tack"/>
+//               <i className = "fa fa-times-circle"/>
+//           </span>
+//         </div>
+//     </div>
+//    </div>
+
+// 	);
+// }
+
+// ReactDOM.render(
+//     <CardDisplay {Header} = CardRacks />
+//   document.getElementById('root')
+// );
 
 
 
